9b55ccc1ad41d57924401b3767c8625ea0990586
jrh3k5 <jrh3k5@gmail.com>
2017-09-19 20:53:05 -0500

    Fully flesh out script

--- src/etc/sample-project/run.sh
+++ src/etc/sample-project/run.sh
@@ -1,6 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script is largely targeted at fresh Ubuntu docker images
 # Before attempting to install git, make sure to run:
 #  apt-get update
 #  apt-get install git
+#  git config --global user.name "jrh3k5"
+#  git config --global user.email "jrh3k5@gmail.com"
+#  apt-get install default-jdk
+#  apt-get install maven
+#
+#  From the root directory of the project, run:
+#    mvn clean install
+#  ...to get the plugin installed in the local repo
 
+mvn flume-agent:run
